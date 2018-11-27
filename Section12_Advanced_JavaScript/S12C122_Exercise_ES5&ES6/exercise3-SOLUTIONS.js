0aa4bf02b3575ee0c9ed0a92cf0255ffd7e1f33d
Beto Mina <yuberalberto@gmail.com>
2018-11-27 00:10:59 -0500

    minor changes

--- Section12_Advanced_JavaScript/S12C122_Exercise_ES5&ES6/exercise3-SOLUTIONS.js
+++ Section12_Advanced_JavaScript/S12C122_Exercise_ES5&ES6/exercise3-SOLUTIONS.js
@@ -43,6 +43,6 @@ const whereAmI = (username, location) => {
     if (username && location) {
         return "I am not lost";
     } else {
-        return "I am totally lost!":
+        return "I am totally lost!";
     }
 }
--- Section12_Advanced_JavaScript/S12C124_Exercises_AdvancedFunctions/exercise4-SOLUTIONS.js
+++ Section12_Advanced_JavaScript/S12C124_Exercises_AdvancedFunctions/exercise4-SOLUTIONS.js
@@ -27,5 +27,5 @@ const add5 = (num) => num + 5;
 compose(add1, add5)(10) // 16
 
 //What are the two elements of a pure function?
-1. Deterministic --> always produces the same results given the same inputs
-2. No Side Effects -->  It does not depend on any state, or data, change during a program’s execution. It must only depend on its input elements.
+//1. Deterministic --> always produces the same results given the same inputs
+//2. No Side Effects -->  It does not depend on any state, or data, change during a program’s execution. It must only depend on its input elements.
