e9aa2584248d9279a0efd952af84a5091484c382
Beto Mina <yuberalberto@gmail.com>
2018-12-04 11:37:09 -0500

    point 1 solved 95%

--- Section13_Advanced_JavaScript/S13C155_OptionalExercise_JavascriptLogic/script.js
+++ Section13_Advanced_JavaScript/S13C155_OptionalExercise_JavascriptLogic/script.js
@@ -1,21 +1,30 @@
-const a = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
+const a = [1,2,4,591,"2",392,391,"a","3",2,"a","a",5,10,2,1,1,1,20,20,1,1,391];
 
 function organize(arr=[]){
-    const newArr = arr.sort(function(a, b){return a - b});
+    const newArr=[];
+    newArr[0] = arr.sort(function(a, b){return a - b}).filter(value=> typeof value==="number");
+    newArr[1] = arr.sort(function(a, b){return a - b}).filter(value=> typeof value==="string");
+    console.log('newArr', newArr);
+
     let myArr = [];
-    for(let i=0; i<newArr.length; i++){
-        aux=[newArr[i]];
-        while(newArr[i+1]===newArr[i]){
-            aux.push(newArr[i]);
-            i++;
+
+    for(let j=0; j<newArr.length; j++){
+        // debugger;
+        for(let i=0; i<newArr[j].length; i++){
+            let aux=[];
+            aux = [newArr[j][i]];
+            while(newArr[j][i+1]===newArr[j][i]){
+                aux.push(newArr[j][i]);
+                i++;
+            }
+            if(aux.length===1){
+                myArr.push(aux[0])
+            }
+            else{
+                myArr.push(aux);
+            }
         }
-        
-        if(aux.length===1){
-            myArr.push(aux[0])
-        }
-        else{
-            myArr.push(aux);
-        }
+
     }
 
     return myArr;
